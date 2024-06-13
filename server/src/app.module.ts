1d6d1aeccc1b995d71fbf1815029e75043523268
Кирилл Вербин <kirill.verbin@volga.rt.ru>
2024-06-13 17:30:49 +0300

    Intit project: 1) Init nest project 2) Add Config module 3) Add Mongoose and connect to db 4) Add docker compose with mongo-db

---
+++ server/src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+
+import { ConfigModule } from '@nestjs/config'
+import { MongooseModule } from '@nestjs/mongoose';
+import { DocModule } from './doc/doc.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.DATABASE_URL),
+    DocModule
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
